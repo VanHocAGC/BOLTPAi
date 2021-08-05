617de42d6acc9f851141c02e0b8261cffc65726b
vanhocAGC <vovanhoc12@gmail.com>
2021-08-05 15:11:49 +0700

    done news

---
+++ action.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#slick-slide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:$('#news-prev'),
+        nextArrow:$('#news-next')
+    })
+})
