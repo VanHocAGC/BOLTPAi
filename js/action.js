750edbcff29b94d4e2ebf235c41705b58513932e
vanhocAGC <vovanhoc12@gmail.com>
2021-08-11 11:04:38 +0700

    done responsive

--- js/action.js
+++ js/action.js
@@ -3,6 +3,36 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow:$('#news-prev'),
-        nextArrow:$('#news-next')
+        nextArrow:$('#news-next'),
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+          ]
     })
+})
+
+const button = document.querySelector('.mobile-button')
+const aboutp = document.querySelectorAll('.about-item p')
+button.addEventListener('click', () =>{
+    aboutp.forEach(element => {
+        if(element.classList.contains('disable')){
+            element.classList.remove('disable')
+        }else{
+            element.classList.add('disable')
+        }
+    });
 })
